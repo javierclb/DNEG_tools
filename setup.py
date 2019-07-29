c8304ea58ee06abda8c449fc0e1023f65b9a306f
javierclb <javierclb>
2019-07-29 11:35:30 -0400

    new repository2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='PLP_Output',
-    version='0.0.4',
-    description='Libreria para tratamiento de salidas PLP',
-    packages=['PLP_Output'],
+    name='DNEG_tools',
+    version='0.0.1',
+    description='Set de funciones y clases para trabajo de Division Negocios de Colbun',
+    packages=['DNEG_tools'],
     author='Javier De la Fuente Arce',
     author_email='javier.delafuente18@outlook.com',
-    url='https://github.com/javierclb/PLP-Output',
-    download_url='https://github.com/javierclb/PLP-Output/tree/master/dist',
+    url='https://github.com/javierclb/DNEG_tools',
+    download_url='https://github.com/javierclb/DNEG_tools/tree/master/dist',
     keywords=['PLP','energy','CDEC'],
     licence='MIT',
     install_requires=['pandas']
